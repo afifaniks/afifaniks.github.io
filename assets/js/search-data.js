6a3a1db3d70db3658c77efe029fb50436a1a8f38
afifaniks <afifanik@gmail.com>
2024-12-31 01:55:47 +0000

    Deploying to gh-pages from @ afifaniks/afifaniks.github.io@ad7a12e74ead8319fbebf76a42eade08dac7ac26 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
