d398a7eb89c2f49965b6de5f37dbd0f4899121ec
Afif Al Mamun <afifanik@gmail.com>
2026-05-16 00:27:59 -0400

    Udate image preview style

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -3,4 +3,27 @@ $(document).ready(function () {
   medium_zoom = mediumZoom("[data-zoomable]", {
     background: getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color") + "ee", // + 'ee' for trasparency.
   });
+
+  // Publication preview full-screen lightbox.
+  var $overlay = $('<div class="pub-preview-overlay"><button class="pub-preview-close" aria-label="Close">&times;</button><img></div>').appendTo("body");
+  var $overlayImg = $overlay.find("img");
+
+  function closeOverlay() {
+    $overlay.removeClass("active");
+    $overlayImg.attr("src", "");
+  }
+
+  $(document).on("click", ".pub-preview-link", function (e) {
+    e.preventDefault();
+    $overlayImg.attr("src", $(this).attr("href"));
+    $overlay.addClass("active");
+  });
+
+  $overlay.on("click", function (e) {
+    if (!$(e.target).is("img")) closeOverlay();
+  });
+
+  $(document).on("keydown", function (e) {
+    if (e.key === "Escape") closeOverlay();
+  });
 });
